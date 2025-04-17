bd3a4a9b42dc3c974c98e9ba9df9f191c610dbea
f80824568 <f80824568@gmail.com>
2025-04-17 16:31:47 -0500

    Deploy latest changes

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Reconstruir archivos desde base64 si existen
+if (process.env.GOOGLE_CREDENTIALS_B64 && process.env.GOOGLE_TOKEN_B64) {
+  fs.writeFileSync('credentials.json', Buffer.from(process.env.GOOGLE_CREDENTIALS_B64, 'base64').toString());
+  fs.writeFileSync('token.json', Buffer.from(process.env.GOOGLE_TOKEN_B64, 'base64').toString());
+}
+
+
 import express from 'express';
 import dotenv from 'dotenv';
 import { getAvailableSlots, bookSlot } from './calendarService.js';
